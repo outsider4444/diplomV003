a08ed98feac10dde524a1828cb775ce0b27b57bf
outsider4444 <danila_kuzin_2001@mail.ru>
2021-04-06 13:28:39 +0400

    попытка номер 5

--- diplomV003/settings.py
+++ diplomV003/settings.py
@@ -133,10 +133,14 @@ LOGIN_REDIRECT_URL = "/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SITE_ID = 1
