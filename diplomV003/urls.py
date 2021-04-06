14710a821d8888f4febd5f8bd7e8381edeef6c9c
outsider4444 <danila_kuzin_2001@mail.ru>
2021-04-06 14:26:32 +0400

    попытка номер 7

--- diplomV003/urls.py
+++ diplomV003/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
